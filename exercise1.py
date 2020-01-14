83a34211a3472ed0955910f4bbbb871740e033e1
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-14 15:59:19 -0600

    End of class

---
+++ exercise1.py
@@ -0,0 +1,47 @@
+# 1 Tip Calculator
+# bill_amount = input("Total bill amount?" )
+# service_qual = input("Level of service: good, fair, or bad?" )
+# bill_amount = float(bill_amount)
+
+# if service_qual == "good":
+#     tip = bill_amount * 0.2
+#     print("Tip amount: $" + "%.2f" % tip)
+# elif service_qual == "fair":
+#     tip = bill_amount * 0.15
+#     print("Tip amount: $" + "%.2f" % tip)
+# elif service_qual == "bad":
+#     tip = bill_amount * 0.1
+#     print("Tip amount: $" + "%.2f" % tip)
+# else: 
+#     print("You are not an elite Yelper.")
+
+# total = bill_amount + tip
+# print("Total amount: $" + "%.2f" % total)
+
+#2 Tip Calculator 2
+# bill_amount = input("Total bill amount?" )
+# service_qual = input("Level of service: good, fair, or bad?" )
+# num_splits = input("Split how many ways?" )
+# bill_amount = float(bill_amount)
+# num_splits = int(num_splits)
+
+# if service_qual == "good":
+#     tip = bill_amount * 0.2
+#     print("Tip amount: $" + "%.2f" % tip)
+# elif service_qual == "fair":
+#     tip = bill_amount * 0.15
+#     print("Tip amount: $" + "%.2f" % tip)
+# elif service_qual == "bad":
+#     tip = bill_amount * 0.1
+#     print("Tip amount: $" + "%.2f" % tip)
+# else: 
+#     print("You are not an elite Yelper.")
+
+# total = bill_amount + tip
+# amt_per_person = total / num_splits
+# print("Total amount: $" + "%.2f" % total)
+# print("Amount per person: $" + "%.2f" % amt_per_person)
+
+#3 Coins
+coins = 0
+while 
