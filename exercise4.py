08b332e91a3822847829ac4c2d01dfb6e822129c
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-17 15:35:25 -0600

    2 added

--- exercise4.py
+++ exercise4.py
@@ -1,9 +1,13 @@
 #1 Letter Summary
-word = input("Enter a word >>")
+# word = input("Enter a word >>")
 
-empty_dict = {}
-for letters in word:
-    if letters not in empty_dict:
-        empty_dict[letters] = 0
-    empty_dict[letters] += 1
-print(empty_dict)
+# empty_dict = {}
+# for letters in word:
+#     if letters not in empty_dict:
+#         empty_dict[letters] = 0
+#     empty_dict[letters] += 1
+# print(empty_dict)
+
+#2 Word Summary
+
+
