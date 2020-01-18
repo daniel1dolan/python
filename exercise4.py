03fc7d6747ef70f60c87de8effda6f0b5834592b
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-18 15:22:13 -0600

    2 completed

--- exercise4.py
+++ exercise4.py
@@ -9,5 +9,23 @@
 # print(empty_dict)
 
 #2 Word Summary
+user_input = input("Please enter a sentence (include a period or space at the end): ")
+
+empty_dict = {}
+word_dump = ""
+for letter in user_input:
+    if letter == " "  or letter == ".":
+        if word_dump not in empty_dict:
+            empty_dict[word_dump] = 0
+            empty_dict[word_dump] += 1
+            word_dump = ""
+        else:
+            empty_dict[word_dump] += 1
+            word_dump = ""
+    else: 
+        word_dump += letter
+print(empty_dict)
+    
+
 
 
