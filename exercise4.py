9cdb4eeae3fda8d2da16481d18f76c29817ed8b9
Daniel Dolan <daniel.dolan654321@gmail.com>
2020-01-20 20:54:27 -0600

    Completed homework

--- exercise4.py
+++ exercise4.py
@@ -9,6 +9,24 @@
 # print(empty_dict)
 
 #2 Word Summary
+# user_input = input("Please enter a sentence (include a period or space at the end): ")
+
+# empty_dict = {}
+# word_dump = ""
+# for letter in user_input:
+#     if letter == " "  or letter == ".":
+#         if word_dump not in empty_dict:
+#             empty_dict[word_dump] = 0
+#             empty_dict[word_dump] += 1
+#             word_dump = ""
+#         else:
+#             empty_dict[word_dump] += 1
+#             word_dump = ""
+#     else: 
+#         word_dump += letter
+# print(empty_dict)
+
+#3 Sorting a Histogram
 user_input = input("Please enter a sentence (include a period or space at the end): ")
 
 empty_dict = {}
@@ -24,8 +42,29 @@ for letter in user_input:
             word_dump = ""
     else: 
         word_dump += letter
-print(empty_dict)
+
+top1 = [0, 0]
+top2 = [0, 0]
+top3 = [0, 0]
+dict__to_tuple = empty_dict.items()
+for words in dict__to_tuple:
+    if words[1] > top1[1]:
+        top1[0] = words[0]
+        top1[1] = words[1]
+    elif words[1] > top2[1]:
+        top2[0] = words[0]
+        top2[1] = words[1]
+    elif words[1] > top3[1]:
+        top3[0] = words[0]
+        top3[1] = words[1]
+print("The top three words are:")
+print(top1)
+print(top2)
+print(top3)
+
     
+
+
 
 
 
